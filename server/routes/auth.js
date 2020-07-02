1524901a9b5d8371c88b5eab74a447ddf2f92ecb
Erwan Vallée <erwanp.vallee@gmail.com>
2020-07-02 18:28:30 +0200

    maj bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/user.model');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
